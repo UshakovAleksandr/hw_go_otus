afbc74fe6c622cc678e79c783a8b80bdbec31739
ruauka <sashapimp@mail.ru>
2022-04-24 17:45:00 +0300

    manage goleak

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,138 +10,65 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// wg
-	wg := sync.WaitGroup{}
-	// в него пишем таски
-	chIn := make(chan func() error)
-	// из него читаем ошибки для коллекции
+	// define chans
+	chIn := make(chan Task)
 	chErr := make(chan error)
-	// тех канал. Из него вычитывают nil все горутины после max err
-	chDone := make(chan struct{})
+	chTooManyErrs := make(chan struct{})
+	chAllDone := make(chan struct{})
 
-	// цикл запуска воркеров
+	// run workers
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
 	for i := 0; i < n; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			// воркеры по очереди вытаскивают таски из chIn
 			for task := range chIn {
-				// проверяем таску на err
 				if err := task(); err != nil {
-					// если err, то пишем в chErr
 					select {
 					case chErr <- err:
-					// когда max err, работающие воркеры вычитывают
-					// из канала nil и останавливаются.
-					// В канала nil, тк нет писателей
-					case <-chDone:
+					case <-chTooManyErrs:
 						return
 					}
 				}
 			}
 		}()
 	}
-	// пишем таски в канал
-	go func() {
-		for _, v := range tasks {
-			chIn <- v
-		}
-		// закрываем для range
-		close(chIn)
-	}()
 
-	// запускаем в отдельной горутине для закрытия канала chErr
-	// после остановки воркеров
+	// close chAllDone when all workers exited
 	go func() {
 		wg.Wait()
-		close(chErr)
+		close(chAllDone)
 	}()
 
-	maxErrorsCount := m
-	var counter int
+	// feed input channel with tasks
+	go func() {
+		defer close(chIn)
+		for _, task := range tasks {
+			select {
+			case chIn <- task:
+			case <-chTooManyErrs:
+				// stop when too many errors
+				return
+			}
+		}
+	}()
 
-	// вычитываем из chErr без дедлока, тк выше wg.Wait() запущена в горутине
-	for i := range chErr {
-		fmt.Println(i)
-		counter++
-		if counter == maxErrorsCount {
-			// закрываем тех канал
-			close(chDone)
-			return ErrErrorsLimitExceeded
+	// count error
+	errCounter := 0
+	for {
+		select {
+		case <-chErr:
+			errCounter++
+			if errCounter >= m {
+				close(chTooManyErrs)
+				fmt.Printf("too many errs: %d\n", errCounter)
+				return ErrErrorsLimitExceeded
+			}
+		case <-chAllDone:
+			fmt.Println("all done with success")
+			return nil
 		}
 	}
-	return nil
 }
-
-// делал на этом примере, randErr() использовал для рандомной ошибки
-// мысль была такая
-
-// type Task func() error
-//
-// func randErr() error {
-//	rand.Seed(time.Now().UnixNano())
-//	a := rand.Intn(2-0) + 0
-//	if a == 0 {
-//		return fmt.Errorf("%v", "ошибка")
-//	}
-//	return nil
-//}
-//
-// func Run() {
-//	var tasks []Task
-//	for i := 0; i < 10; i++ {
-//		tasks = append(tasks, func() error {
-//			return randErr()
-//		})
-//	}
-//
-//	wg := sync.WaitGroup{}
-//	chIn := make(chan func() error)
-//	chErr := make(chan error)
-//	chDone := make(chan struct{})
-//
-//	for i := 0; i < 4; i++ {
-//		wg.Add(1)
-//		go func() {
-//			defer wg.Done()
-//			for task := range chIn {
-//				if err := task(); err != nil {
-//					select {
-//					case chErr <- err:
-//					case <-chDone:
-//						return
-//					}
-//				}
-//			}
-//		}()
-//	}
-//
-//	go func() {
-//		for _, v := range tasks {
-//			chIn <- v
-//		}
-//		close(chIn)
-//	}()
-//
-//	// wg дожедается отраблтки всех воркеров
-//	go func() {
-//		wg.Wait()
-//		close(chErr)
-//	}()
-//
-//	a := 4
-//	var counter int
-//
-//	for i := range chErr {
-//		fmt.Println(i)
-//		counter++
-//		if counter == a {
-//			close(chDone)
-//			break
-//		}
-//	}
-//	// проверял второй wg.Wait() точно завершились ли все горутины, если не висит, то ок
-//	//wg.Wait()
-//}
