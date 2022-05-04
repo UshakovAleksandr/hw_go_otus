1834fa6aeccecac68579028b7fcf2df8dde697bc
Ушаков Александр Сергеевич <AS1Ushakov@sberbank.ru>
2022-05-04 16:55:50 +0300

    third variant

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,12 @@
 package hw06pipelineexecution
 
+import (
+	"runtime"
+	"sort"
+	"sync"
+	"sync/atomic"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -8,33 +15,56 @@ type (
 
 type Stage func(in In) (out Out)
 
-func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	ch := make(Bi)
+func worker(in In, out Bi, done In, wg *sync.WaitGroup, ops *uint64, stages ...Stage) {
+	defer wg.Done()
+	for i := range stages[3](stages[2](stages[1](stages[0](in)))) {
+		select {
+		case <-done:
+			return
+		case out <- i:
+			atomic.AddUint64(ops, 1)
+		}
+	}
+}
 
-	select {
-	case <-done:
-		close(ch)
-		return ch
-	default:
-		return stages[3](stages[2](stages[1](stages[0](in))))
+func ExecutePipeline(in In, done In, stages ...Stage) Out {
+	chForSort := make(Bi)
+	sortedCh := make(Bi)
+	toReturn := make(Bi)
+	close(toReturn)
+
+	wg := sync.WaitGroup{}
+
+	var counter uint64
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			worker(in, chForSort, done, &wg, &counter, stages...)
+		}()
 	}
 
-	// изменил немного, ошибку предыдушего коммента понял
-	// но второй тест по прежнему не могу пройти. Причины тоже понятны, неправильный select.
-	// Если я правильно понимаю, то принцип должен быть такой:
-	// перед select нужжен for{}, в котором постоянно проверяется передаваемое значение обоих каналов
-	// записываем полученные из стейджей значения, сколько успеет выполнится и если done стал закрыт, то нужно прибить эти
-	// значения и отправить закрытый канал
-	//
-	//for {
-	//	select {
-	//	case <-done:
-	//		close(ch)
-	//		return ch
-	//	default:
-	//		someCh <- stages[3](stages[2](stages[1](stages[0](in))))
-	//      ...
-	//	}
-	//}
-	// return ...
+	go func() {
+		wg.Wait()
+		close(chForSort)
+	}()
+
+	var resultSl []string
+
+	for item := range chForSort {
+		resultSl = append(resultSl, item.(string))
+	}
+
+	if counter == 5 {
+		sort.Strings(resultSl)
+
+		go func() {
+			defer close(sortedCh)
+			for _, item := range resultSl {
+				sortedCh <- item
+			}
+		}()
+		return sortedCh
+	}
+	return toReturn
 }
